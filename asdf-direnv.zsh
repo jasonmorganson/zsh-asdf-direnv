45923f4448efad817ae36b2153a089bcc4aa61a3
Jason Morganson <jason@cherre.com>
2021-01-11 21:28:39 -0500

    Use zsh hook

--- asdf-direnv.zsh
+++ asdf-direnv.zsh
@@ -1,5 +1,5 @@
 # Hook direnv into your shell.
-eval "$(asdf exec direnv hook bash)"
+eval "$(asdf exec direnv hook zsh)"
 
 # A shortcut for asdf managed direnv.
 direnv() { asdf exec direnv "$@"; }
